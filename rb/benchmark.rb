2405913242b42dfb2bf68e8cc870d5a572c8abd0
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-02 00:38:22 -0400

    Update benchmark task

--- rb/benchmark.rb
+++ rb/benchmark.rb
@@ -1,8 +1,9 @@
 require 'benchmark'
 require_relative 'insertion_sort'
+require_relative 'selection_sort'
 
 # a = sample array of size (n)
-n_sample = rand(1..1000)
+n_sample = rand(1..10000)
 a = (1..n_sample).to_a.shuffle
 expect_output = a.sort
 
@@ -10,4 +11,5 @@ Benchmark.bmbm do |x|
   x.report("#sort:") {a.sort}
   x.report("#sort!:") {a.sort!}
   x.report("#ins_sort:") {ins_sort(a)}
+  x.report("#sel_sort:") {sel_sort(a)}
 end
