d4e22b1f20089fc610e4f8f3060258856f7401ee
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-03 17:26:37 -0400

    Add loop to build n sized sample array

---
+++ js/insertion_sort.js
@@ -0,0 +1,7 @@
+// building a sample array of size (n)
+var a = []
+var nSample = Math.round(Math.random() * 100)
+for (var i=0; i < nSample; i++) {
+  a.push(Math.round(Math.random() * nSample))
+}
+
