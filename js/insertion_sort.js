68275a9d1a8f72d53328b965453c86d5241dc850
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-03 17:58:29 -0400

    Add insertion sort in JS

--- js/insertion_sort.js
+++ js/insertion_sort.js
@@ -5,3 +5,14 @@ for (var i=0; i < nSample; i++) {
   a.push(Math.round(Math.random() * nSample))
 }
 
+// insertion sort
+var insertionSort = function(a) {
+  for (var i = 0; i < a.length; i++) {
+    var curr = a[i];
+    for (var idx = i - 1; idx >= 0 && a[idx] > curr; idx--) {
+      a[idx + 1] = a[idx];
+    }
+    a[idx+1] = curr;  
+  }
+  return a
+}
