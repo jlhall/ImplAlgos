5bc57a0bb0b9d81fe4f32b38a30004c1edf1fd9f
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-04 22:21:08 -0400

    Add basic insertion sort Python, needs random array creation

---
+++ py/insertion_sort.py
@@ -0,0 +1,12 @@
+ab = [3, 5, 1,  2, 1, 11]
+def ins_sort(a):
+  for i in range(1, len(a)):
+    curr_val = a[i]
+    replace_idx = i
+    while replace_idx > 0 and a[replace_idx - 1] > curr_val:
+      a[replace_idx] = a[replace_idx - 1]
+      replace_idx = replace_idx - 1
+    a[replace_idx] = curr_val 
+  return a
+
+print ins_sort(ab)
