3079794e508b8aa3851685724a7179d05362ee46
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-06 22:58:23 -0400

    Add bubble sort to benchmark file

--- rb/benchmark.rb
+++ rb/benchmark.rb
@@ -1,6 +1,7 @@
 require 'benchmark'
 require_relative 'insertion_sort'
 require_relative 'selection_sort'
+require_relative 'bubble_sort'
 
 # a = sample array of size (n)
 n_sample = rand(1..10000)
@@ -12,4 +13,5 @@ Benchmark.bmbm do |x|
   x.report("#sort!:") {a.sort!}
   x.report("#ins_sort:") {ins_sort(a)}
   x.report("#sel_sort:") {sel_sort(a)}
+  x.report("#bub_sort:") {bub_sort(a)}
 end
