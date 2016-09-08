f2aefb7aec75aaa337d03f920687200e1a97fc65
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-07 21:32:03 -0400

    Add shell sort to benchmark file

--- rb/benchmark.rb
+++ rb/benchmark.rb
@@ -2,11 +2,15 @@ require 'benchmark'
 require_relative 'insertion_sort'
 require_relative 'selection_sort'
 require_relative 'bubble_sort'
+require_relative 'shell_sort'
+print "\n"
 
 # a = sample array of size (n)
-n_sample = rand(1..10000)
+n_sample = rand(2..10000)
 a = (1..n_sample).to_a.shuffle
 expect_output = a.sort
+print "Test Array Size: #{a.length}\n"
+print "\n"
 
 Benchmark.bmbm do |x|
   x.report("#sort:") {a.sort}
@@ -14,4 +18,5 @@ Benchmark.bmbm do |x|
   x.report("#ins_sort:") {ins_sort(a)}
   x.report("#sel_sort:") {sel_sort(a)}
   x.report("#bub_sort:") {bub_sort(a)}
+  x.report("#shell_sort:") {shell_sort(a)}
 end
