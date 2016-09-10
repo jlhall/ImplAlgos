8696ade2da8550acaede61d718d2202f2a90a9b2
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-09 22:20:01 -0400

    Added notes to Python merge_sort implementation

--- py/merge_sort.py
+++ py/merge_sort.py
@@ -5,6 +5,10 @@ ab = []
 for i in range(n):
   ab.append(random.randint(1, n))
 
+# my original implementation tried to use a helper method for stitching together the sorted results of the recursion but I hit Python's recursion lock... not good. Adapted the solution discussed here as it made much more sense: http://interactivepython.org/runestone/static/pythonds/SortSearch/TheMergeSort.html
+
+# a counter for each half keeps track of your position in that half as you compare across, while a result counter allows you to build the final result array (as you "merge"/"stitch").
+
 def merge_sort(a):
     if len(a)>1:
         mid = len(a)//2
