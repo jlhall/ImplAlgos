58545e3b9f98d5419bacd8bc9acb24acc99d2dfa
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-11 21:48:22 -0400

    Add quick sort to faux spec and benchmark files

--- rb/benchmark.rb
+++ rb/benchmark.rb
@@ -5,10 +5,11 @@ require_relative 'bubble_sort'
 require_relative 'shell_sort'
 require_relative 'merge_sort'
 require_relative 'heap_sort'
+require_relative 'quick_sort'
 print "\n"
 
 # a = sample array of size (n)
-n_sample = rand(2..10000)
+n_sample = rand(200000..1000000)
 a = (1..n_sample).to_a.shuffle
 expect_output = a.sort
 print "Test Array Size: #{a.length}\n"
@@ -23,4 +24,5 @@ Benchmark.bmbm do |x|
   x.report("#shell_sort:") {shell_sort(a)}
   x.report("#merge_sort:") {merge_sort(a)}
   x.report("#heap_sort:") {heap_sort(a)}
+  x.report("#quick_sort:") {quick_sort(a)}
 end
