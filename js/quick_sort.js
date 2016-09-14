aeb31aafb4247b3d96165d5e8cca6ba94efb9dd3
Jonathan Hall <ohsnapjon@gmail.com>
2016-09-13 22:05:20 -0400

    Add quick sort in JS

---
+++ js/quick_sort.js
@@ -0,0 +1,19 @@
+// building a sample array of size (n)
+var a = []
+var nSample = Math.round(Math.random() * 100)
+for (var i=0; i < nSample; i++) {
+  a.push(Math.round(Math.random() * nSample))
+};
+
+// Quick Sort
+var quick_sort = function(a) {
+    if (a.length <= 1) return a;
+
+    var left = [], right = [], pivot = a[0];
+
+    for (var i = 1; i < a.length; i++) {
+        a[i] < pivot ? left.push(a[i]) : right.push(a[i]);
+    };
+
+    return quick_sort(left).concat(pivot, quick_sort(right));
+};
